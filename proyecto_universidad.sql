7bee2101c4ecd3f266c8f3a8710d649aac13b30b
rbespada <rbuendiaespada@gmail.com>
2025-08-08 10:14:01 +0200

    Base de datos creada

---
+++ proyecto_universidad.sql
@@ -0,0 +1,9 @@
+-- ===========================================
+-- CREACIÓN DE LA BASE DE DATOS
+-- ===========================================
+
+-- Si no existe, se crea una base de datos llamada Universidad
+CREATE DATABASE IF NOT EXISTS Universidad;
+
+-- Activamos esta base de datos para usarla a partir de ahora
+USE Universidad;

6e8dc35f688b990911c98e218439ed1edbacc161
rbespada <rbuendiaespada@gmail.com>
2025-08-08 12:03:19 +0200

    Creadas todas las tablas subtipos ISA y archivo SVG del MERE

--- proyecto_universidad.sql
+++ proyecto_universidad.sql
@@ -41,4 +41,67 @@ CREATE TABLE TELEFONO (
 );
 
 
+-- ===========================================
+-- 3. TABLA ALUMNO (subtipo de PERSONA)
+-- ===========================================
 
+CREATE TABLE ALUMNO (
+    dni CHAR(9) PRIMARY KEY,
+    especialidad ENUM('Ingeniería', 'Filología', 'Biología', 'Medicina', 'Humanidades') NOT NULL,  --Elige especialidad
+    FOREIGN KEY (dni) REFERENCES PERSONA(dni)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+-- ===========================================
+-- 4. TABLA PROFESOR (subtipo de PERSONA)
+-- ===========================================
+
+CREATE TABLE PROFESOR (
+    dni CHAR(9) PRIMARY KEY,
+    salario_anual DECIMAL(8,2) NOT NULL,  --Permite 8 digitos en total y dos de ellos decimales, como por ejemplo 599999,95
+    FOREIGN KEY (dni) REFERENCES PERSONA(dni)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CHECK (salario_anual > 0 AND salario_anual <= 60000)  --Revisa que sea mayor que 0 y menor o igual que 60000
+);
+
+
+-- ===========================================
+-- 5. TABLA ADMINISTRATIVO (subtipo de PERSONA)
+-- ===========================================
+
+CREATE TABLE ADMINISTRATIVO (
+    dni CHAR(9) PRIMARY KEY,
+    especializacion VARCHAR(50) NOT NULL,
+    FOREIGN KEY (dni) REFERENCES PERSONA(dni)
+        ON DELETE CASCADEI
+        ON UPDATE CASCADE
+);
+
+
+-- ===========================================
+-- 6. TABLA AUDITOR (subtipo de PERSONA)
+-- ===========================================
+
+CREATE TABLE AUDITOR (
+    dni CHAR(9) PRIMARY KEY,
+    años_experiencia TINYINT UNSIGNED NOT NULL,
+    trabaja_fuera BOOLEAN NOT NULL,   -- Especifica entre SI o NO valor booleano 1,0
+    FOREIGN KEY (dni) REFERENCES PERSONA(dni)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+-- ===========================================
+-- 7. TABLA VISITA (subtipo de PERSONA)
+-- ===========================================
+
+CREATE TABLE VISITA (
+    dni CHAR(9) PRIMARY KEY,
+    FOREIGN KEY (dni) REFERENCES PERSONA(dni)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
